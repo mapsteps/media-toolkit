0fda1c8686d0425630e8c67d6404467fb5e16637
MapSteps <connect@mapsteps.com>
2023-11-30 14:31:50 +0100

    uppercase

--- templates/metaboxes/template-tags.php
+++ templates/metaboxes/template-tags.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 <div class="heatbox mediatk-tags-metabox">
 	<h2><?php _e( 'Template tags', 'media-toolkit' ); ?></h2>
 	<div class="heatbox-content">
-		<p><?php _e( 'Available template tags to create a custom filename structure. <br><strong>(click to copy)</strong>', 'media-toolkit' ); ?></p>
+		<p><?php _e( 'Available template tags to create a custom filename structure. <br><strong>(Click to copy)</strong>', 'media-toolkit' ); ?></p>
 		<p class="tags-wrapper">
 			<code>{author_first_name}</code>, <code>{author_last_name}</code>, <code>{author_full_name}</code>, <code>{original_file_name}</code>, <code>{upload_timestamp}</code>, <code>{upload_date}</code>, <code>{random_id}</code>
 		</p>
