d51df9df239c2b127708b439b7d9b0e4ede84ac0
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-12-01 20:08:39 +0700

    use div, not p

--- templates/metaboxes/template-tags.php
+++ templates/metaboxes/template-tags.php
@@ -15,8 +15,14 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 	</h2>
 	<div class="heatbox-content">
 		<p><?php _e( 'Available template tags to create a custom filename structure. <br><strong>(Click to copy)</strong>', 'media-toolkit' ); ?></p>
-		<p class="tags-wrapper">
-			<code>{author_first_name}</code> <code>{author_last_name}</code> <code>{author_full_name}</code> <code>{original_file_name}</code> <code>{upload_timestamp}</code> <code>{upload_date}</code> <code>{random_id}</code>
-		</p>
+		<div class="tags-wrapper">
+			<code>{author_first_name}</code>
+			<code>{author_last_name}</code>
+			<code>{author_full_name}</code>
+			<code>{original_file_name}</code>
+			<code>{upload_timestamp}</code>
+			<code>{upload_date}</code>
+			<code>{random_id}</code>
+		</div>
 	</div>
 </div>
