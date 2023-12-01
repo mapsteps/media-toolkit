c18f10d45b38529c8ff0f12fa6cb19aee13aadf9
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-12-01 20:38:23 +0700

    use execCommand as navigator.clipboard fallback

--- assets-src/js/settings-page.ts
+++ assets-src/js/settings-page.ts
@@ -28,7 +28,7 @@
 		// Copy value to clipboard.
 		copyToClipboard(value);
 
-		const notice = document.createElement(
+		const notice = document.querySelector(
 			".mediatk-tags-metabox .action-status"
 		) as HTMLElement | null;
 		if (!notice) return;
@@ -43,10 +43,35 @@
 	async function copyToClipboard(text: string) {
 		try {
 			await navigator.clipboard.writeText(text);
-			console.log("Text copied to clipboard:", text);
+		} catch (err) {
+			// console.error("Unable to copy text to clipboard:", err);
+			copyToClipboardViaExecCommand(text);
+		}
+	}
+
+	async function copyToClipboardViaExecCommand(text: string) {
+		const textArea = document.createElement("textarea");
+
+		textArea.value = text;
+		textArea.style.position = "fixed";
+		textArea.style.top = "-3px";
+		textArea.style.left = "-3px";
+		textArea.style.width = "1px";
+		textArea.style.height = "1px";
+		textArea.style.background = "transparent";
+		textArea.style.opacity = "0";
+
+		document.body.appendChild(textArea);
+		textArea.focus();
+		textArea.select();
+
+		try {
+			document.execCommand("copy");
 		} catch (err) {
 			console.error("Unable to copy text to clipboard:", err);
 		}
+
+		document.body.removeChild(textArea);
 	}
 
 	function setupFieldsInteraction() {
